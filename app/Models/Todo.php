9f0f5b5d2c1338d52f2ebe86cd4f2638b108c2e5
sigits12 <supratmanto12@gmail.com>
2022-02-03 22:54:37 +0700

    Remove unsused code in Model\Todo

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,24 +17,9 @@ class Todo extends Model
         'title', 'description', 'status', 'user_id', 'start', 'end'
     ];
 
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope('by_user', function (Builder $query) {
-            $query->byUser();
-        });
-    }
-
     public function scopeByUser()
     {
         return $this->where('user_id', auth()->user()->id);
-        // if (!$user) $user = auth()->user();
-        // $query->when($user instanceof User, function (Builder $query) use ($user) {
-        //     $query->whereHas('todos', function (Builder $query) use ($user) {
-        //         $query->byUser($user);
-        //     });
-        // });
     }
 
     public function user()
