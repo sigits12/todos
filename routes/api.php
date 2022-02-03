d8837451a736e9922d5618e473379f1a2787daaf
sigits12 <supratmanto12@gmail.com>
2022-02-03 07:53:40 +0700

    Create register and login endpoint

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register', [App\Http\Controllers\AuthController::class, 'register'])->name('register');
+
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    protected function deleteUser($data)
+    {
+        $user = User::where('email', $data['email'])->first();
+        if ($user) {
+            $user->delete();
+        }
+    }
+
+    /** @test */
+    public function register()
+    {
+        $data = [
+            "name" => "Anggoro",
+            "email" => "anggoro@mail.com",
+            "password" => "password123",
+        ];
+
+        $this->deleteUser($data);
+
+        $response = $this->post(route('register'), $data);
+        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'data' => [
+                'name',
+                'email',
+            ],
+            'access_token',
+            'token_type'
+        ]);
+    }
+
+    /** @test */
+    public function login()
+    {
+        $data = [
+            "name" => "Anggoro",
+            "email" => "anggoro@mail.com",
+            "password" => "password123",
+        ];
+
+        $this->deleteUser($data);
+
+        $this->register();
+
+        $response = $this->post(route('login'), $data);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [
+                'name',
+                'email',
+            ],
+            'access_token',
+            'token_type'
+        ]);
+
+    }
+}
